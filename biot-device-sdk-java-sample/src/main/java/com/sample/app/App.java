e72defcac383ea7b8652b21185dd8d7e587e4a5c
RDA-DATA <rdadata@korea.kr>
2024-07-10 15:49:40 +0900

    2024.07.10 불필요 클래스/파일 제거 및 수정

--- biot-device-sdk-java-sample/src/main/java/com/sample/app/App.java
+++ biot-device-sdk-java-sample/src/main/java/com/sample/app/App.java
@@ -8,6 +8,12 @@ import com.sds.iot.sdk.auth.ita.ItaDirectAuth;
  * Sample App
  * ITA 인증 방법으로 IoT 서버와 Connect 하는 예제
  *
+ * -- 설명
+ * PoC 접속 후 사물 > 사물관리 메뉴에서 생성한 사물을 검색하여 하단의 정보를 입력해주세요.
+ * 입력된 정보를 통해 인증 객체를 생성하고 제공되는 SDK의 IoTClient 클래스를 통해 사물객체를 받습니다.
+ * 해당 사물객체를 연결 및 활성화를 하여 데이터를 받을 수 있는 상태로 전환합니다.
+ * 수집데이터 전송을 위해 해당 사물의 속성명과 속성타입에 맞는 값을 입력하여 json형식의 데이터를 생성합니다.
+ * PoC와 연결된 사물객체를 통해 데이터를 전송합니다.
  */
 public class App
 {
@@ -31,8 +37,8 @@ public class App
             // Send activation message
             try {
                 JsonObject data = new JsonObject();
-                data.addProperty("temperature", 1.1);
-                data.addProperty("humidity", 2.2);
+                data.addProperty("temperature", 1.5);
+                data.addProperty("humidity", 2.5);
 
                 client.sendAttributes("Basic-AttrGroup", data.toString());
             } catch (Exception e) {
